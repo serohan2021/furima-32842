c2dfcf3c8f6eb713c19092a85af141a53a85a9dd
serohan2021 <m90.nnagasaka@gmail.com>
2021-05-12 22:47:24 +0900

    商品出品機能の実装とモデルテスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,26 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name,        length: { maximum: 40 }
+    validates :description, length: { maximum: 1000 }
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :postage_type_id
+      validates :region_id
+      validates :shipping_day_id
+    end
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage_type
+  belongs_to :region
+  belongs_to :shipping_day
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  { 'サンプル試験１:' + Faker::JapaneseMedia::OnePiece.akuma_no_mi }
+    description           { '食べると泳げなくなります。' + Faker::Lorem.sentence }
+    price                 { Faker::Number.between(from: 300, to: 9_999_999) }
+    category_id           { Faker::Number.between(from: 2, to: 11) }
+    condition_id          { Faker::Number.between(from: 2, to: 7) }
+    postage_type_id       { Faker::Number.between(from: 2, to: 3) }
+    region_id             { Faker::Number.between(from: 2, to: 48) }
+    shipping_day_id       { Faker::Number.between(from: 2, to: 4) }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
