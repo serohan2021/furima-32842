c9c203df0d47e82e1b677e81d74da0a877862e69
serohan2021 <m90.nnagasaka@gmail.com>
2021-05-13 12:26:44 +0900

    ビューファイルとテストコードの修正
    レビュー後に、ビューファイルの不要箇所削除とテストコードのinteger型の代入値表現を修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,32 +11,32 @@ RSpec.describe Item, type: :model do
     end
 
     it '商品の状態についての入力が ２〜7 であれば、商品の出品ができる' do
-      @item.condition_id = '7'
+      @item.condition_id = 7
       expect(@item).to be_valid
     end
 
     it '商品のカテゴリーについての入力が ２〜11 であれば、商品の出品ができる' do
-      @item.category_id = '11'
+      @item.category_id = 11
       expect(@item).to be_valid
     end
 
     it '配送料の負担についての入力が ２〜３ であれば、商品の出品ができる' do
-      @item.postage_type_id = '3'
+      @item.postage_type_id = 3
       expect(@item).to be_valid
     end
 
     it '発送元の地域についての入力が ２〜48 であれば、商品の出品ができる' do
-      @item.region_id = '48'
+      @item.region_id = 48
       expect(@item).to be_valid
     end
 
     it '発送までの日数の入力が ２〜4 であれば、商品の出品ができる' do
-      @item.shipping_day_id = '4'
+      @item.shipping_day_id = 4
       expect(@item).to be_valid
     end
 
     it '価格の範囲が、¥300 ~ ¥9,999,999 であれば、商品の出品ができる' do
-      @item.price = '300'
+      @item.price = 300
       expect(@item).to be_valid
     end
   end
@@ -73,79 +73,79 @@ RSpec.describe Item, type: :model do
     end
 
     it '商品のカテゴリーが空欄であれば、商品の出品ができない' do
-      @item.category_id = ''
+      @item.category_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Category can't be blank")
     end
 
     it '商品のカテゴリーが１では商品の出品ができない' do
-      @item.category_id = '1'
+      @item.category_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include('Category must be other than 1')
     end
 
     it '商品の状態が空欄であれば、商品の出品ができない' do
-      @item.condition_id = ''
+      @item.condition_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Condition can't be blank")
     end
 
     it '商品の状態が１では商品の出品ができない' do
-      @item.condition_id = '1'
+      @item.condition_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include('Condition must be other than 1')
     end
 
     it '配送料の負担が空欄であれば、商品の出品ができない' do
-      @item.postage_type_id = ''
+      @item.postage_type_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Postage type can't be blank")
     end
 
     it '配送料の負担が１では商品の出品ができない' do
-      @item.postage_type_id = '1'
+      @item.postage_type_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include('Postage type must be other than 1')
     end
 
     it '発送元の地域が空欄であれば、商品の出品ができない' do
-      @item.region_id = ''
+      @item.region_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Region can't be blank")
     end
 
     it '発送元の地域が１では商品の出品ができない' do
-      @item.region_id = '1'
+      @item.region_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include('Region must be other than 1')
     end
 
     it '発送までの日数が空欄であれば、商品の出品ができない' do
-      @item.shipping_day_id = ''
+      @item.shipping_day_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping day can't be blank")
     end
 
     it '発送までの日数が１では商品の出品ができない' do
-      @item.shipping_day_id = '1'
+      @item.shipping_day_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include('Shipping day must be other than 1')
     end
 
     it '価格が空欄であれば、商品の出品ができない' do
-      @item.price = ''
+      @item.price = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Price can't be blank")
     end
 
     it '価格が、¥300より小さい額では商品の出品ができない' do
-      @item.price = '200'
+      @item.price = 200
       @item.valid?
       expect(@item.errors.full_messages).to include('Price is not included in the list')
     end
 
     it '価格が、¥9,999,999より大きい額では商品の出品ができない' do
-      @item.price = '39999999'
+      @item.price = 39999999
       @item.valid?
       expect(@item.errors.full_messages).to include('Price is not included in the list')
     end
