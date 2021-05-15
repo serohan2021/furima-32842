f38891d6afad94e7ef9a8e7749e25782958e3a14
serohan2021 <m90.nnagasaka@gmail.com>
2021-05-15 15:03:28 +0900

    destroyアクション追記
    レビューコメント考慮

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_root, only: [:edit, :update]
+  before_action :move_to_root, only: [:edit, :update, :destroy]
 
   def index
     @item = Item.includes(:user).order('created_at DESC')
